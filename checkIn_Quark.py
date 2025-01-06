4aa26a48bb5a4a71d17fb3b064d76db23d8148ad
baipiao250 <119747138+baipiao250@users.noreply.github.com>
2025-01-06 23:16:07 +0800

    Update checkIn_Quark.py

--- checkIn_Quark.py
+++ checkIn_Quark.py
@@ -1,12 +1,9 @@
-import os 
-import re 
-import sys 
-import requests 
+import os
+import re
+import sys
+import requests
 import subprocess
-
-cookie_list = os.getenv("COOKIE_QUARK").split('\n|&&')
-
-
+import tempfile
 
 # 替代 notify 功能
 def send(title, message):
@@ -14,38 +11,18 @@ def send(title, message):
 
 # 获取环境变量 
 def get_env(): 
-    # 判断 COOKIE_QUARK是否存在于环境变量 
     if "COOKIE_QUARK" in os.environ: 
-        # 读取系统变量以 \n 或 && 分割变量 
-        cookie_list = re.split('\n|&&', os.environ.get('COOKIE_QUARK')) 
+        return re.split('\n|&&', os.environ.get('COOKIE_QUARK')) 
     else: 
-        # 标准日志输出 
         print('❌未添加COOKIE_QUARK变量') 
         send('夸克自动签到', '❌未添加COOKIE_QUARK变量') 
-        # 脚本退出 
         sys.exit(0) 
 
-    return cookie_list 
-
-# 其他代码...
-
 class Quark:
-    '''
-    Quark类封装了签到、领取签到奖励的方法
-    '''
     def __init__(self, user_data):
-        '''
-        初始化方法
-        :param user_data: 用户信息，用于后续的请求
-        '''
         self.param = user_data
 
     def convert_bytes(self, b):
-        '''
-        将字节转换为 MB GB TB
-        :param b: 字节数
-        :return: 返回 MB GB TB
-        '''
         units = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
         i = 0
         while b >= 1024 and i < len(units) - 1:
@@ -54,10 +31,6 @@ class Quark:
         return f"{b:.2f} {units[i]}"
 
     def get_growth_info(self):
-        '''
-        获取用户当前的签到信息
-        :return: 返回一个字典，包含用户当前的签到信息
-        '''
         url = "https://drive-m.quark.cn/1/clouddrive/capacity/growth/info"
         querystring = {
             "pr": "ucpro",
@@ -67,17 +40,12 @@ class Quark:
             "vcode": self.param.get('vcode')
         }
         response = requests.get(url=url, params=querystring).json()
-        #print(response)
         if response.get("data"):
             return response["data"]
         else:
             return False
 
     def get_growth_sign(self):
-        '''
-        获取用户当前的签到信息
-        :return: 返回一个字典，包含用户当前的签到信息
-        '''
         url = "https://drive-m.quark.cn/1/clouddrive/capacity/growth/sign"
         querystring = {
             "pr": "ucpro",
@@ -88,35 +56,25 @@ class Quark:
         }
         data = {"sign_cyclic": True}
         response = requests.post(url=url, json=data, params=querystring).json()
-        #print(response)
         if response.get("data"):
             return True, response["data"]["sign_daily_reward"]
         else:
             return False, response["message"]
 
     def queryBalance(self):
-        '''
-        查询抽奖余额
-        '''
         url = "https://coral2.quark.cn/currency/v1/queryBalance"
         querystring = {
             "moduleCode": "1f3563d38896438db994f118d4ff53cb",
             "kps": self.param.get('kps'),
         }
         response = requests.get(url=url, params=querystring).json()
-        # print(response)
         if response.get("data"):
             return response["data"]["balance"]
         else:
             return response["msg"]
 
     def do_sign(self):
-        '''
-        执行签到任务
-        :return: 返回一个字符串，包含签到结果
-        '''
         log = ""
-        # 每日领空间
         growth_info = self.get_growth_info()
         if growth_info:
             log += (
@@ -146,41 +104,30 @@ class Quark:
 
         return log
 
-
 def main():
-    '''
-    主函数
-    :return: 返回一个字符串，包含签到结果
-    '''
     msg = ""
-    global cookie_quark
     cookie_quark = get_env()
 
     print("✅ 检测到共", len(cookie_quark), "个夸克账号\n")
 
-    i = 0
-    while i < len(cookie_quark):
-        # 获取user_data参数
-        user_data = {}  # 用户信息
-        for a in cookie_quark[i].replace(" ", "").split(';'):
+    for i, cookie in enumerate(cookie_quark):
+        user_data = {}
+        for a in cookie.replace(" ", "").split(';'):
             if not a == '':
                 user_data.update({a[0:a.index('=')]: a[a.index('=') + 1:]})
-        # print(user_data)
-        # 开始任务
         log = f"🙍🏻‍♂️ 第{i + 1}个账号"
         msg += log
-        # 登录
         log = Quark(user_data).do_sign()
         msg += log + "\n"
 
-        i += 1
-
-    # print(msg)
-
     try:
         send('夸克自动签到', msg)
-         # 调用 wxpusher.py 发送通知，并传递签到结果作为参数
-        subprocess.run(["python", "wxpusher.py", msg], check=True)
+        # 将消息写入临时文件
+        with tempfile.NamedTemporaryFile(delete=False) as temp:
+            temp.write(msg.encode('utf-8'))
+            temp_path = temp.name
+        # 调用 wxpusher.py 并传递文件路径
+        subprocess.run(["python", "wxpusher.py", temp_path], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running wxpusher.py: {e}")
     except Exception as err:
@@ -188,9 +135,7 @@ def main():
 
     return msg[:-1]
 
-
 if __name__ == "__main__":
     print("----------夸克网盘开始签到----------")
     main()
     print("----------夸克网盘签到完毕----------")
-
